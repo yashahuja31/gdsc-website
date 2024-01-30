9e0afbd1631a2bf0de04c81debfbf3bba4cc7efb
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-01-30 23:24:36 +0530

    added hero

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -3,8 +3,28 @@ import "./Hero.css"
 export default function Hero() {
 
     return (
-        <div>
+        <div className={"heroBackground"}>
+            <div className={"heroContainer"}>
+                <div className={"heroTitleContainer"}>
+                    <h1><span>G</span>oogle</h1>
+                    <h1><span>D</span>eveloper</h1>
+                    <h1><span>S</span>tudents</h1>
+                    <h1><span>C</span>lub</h1>
+                    <br />
+                    <h1>ADGIPS</h1>
+                </div>
 
+                <div className={"heroImageContainer"}>
+                    <img src={"/hero/sideImg.png"} alt={"image"}/>
+                </div>
+
+            </div>
+            <p>Through GSDC-ADGITM we, Developers and Designers want
+                to foster a healthy development culture. Developer Student
+                Clubs is a program that will help students to learn mobile and
+                web development, design thinking and leadership skills.
+            </p>
         </div>
+
     );
 }
