cbed828d3d55314c802b2e45d653e3f00822e0ca
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-01-31 20:07:49 +0530

    make Header and Hero responsive

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,31 +1,44 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 export default function Header() {
+  const [navStatus, setNavStatus] = useState(false);
+  const toggleNav = () => {
+    setNavStatus(!navStatus);
+    if (!navStatus) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  };
   return (
-    <nav className="full-width-nav">
-      <div className="nav-items-container">
-        <div className="gdscContainer">
-          <div className="logo">
-            <img src="/footer/gdsc.png" alt="gdsc"/>
-          </div>
-          <div className="club">
-            <span className="club-name">google developer students club</span>
-            <span className="clg-name">adgips</span>
-          </div>
-        </div>
-        <div className="nav-menu-links">
-          <a href="/">home</a>
-          <a href="/events">events</a>
-          <a href="/team">team</a>
-        </div>
-        <div className="button-container">
-          <a href="#" className="btn">
-            contact us
-          </a>
-          <a href="#" className="btn">
-            join community
+    <nav>
+      <div className="nav-flex">
+        <div>
+          <a href="/" className="nav-logo">
+            <img src="/footer/gdsc.png" alt="GDSC Logo" />
+            <span>
+              Google Developer Student <br />
+              ADGIPS
+            </span>
           </a>
         </div>
+        <div className={navStatus ? "nav-close" : "nav-open"}>
+          <a href="/">Home</a>
+          <a href="/events">Event</a>
+          <a href="/team">Team</a>
+          <a className="links">Contact Us</a>
+          <a className="links">Join Community</a>
+        </div>
+        <div>
+          <a className="nav-bt1">Contact Us</a>
+          <a className="nav-bt2">Join Community</a>
+        </div>
+        <a onClick={toggleNav} className="bars">
+          <FontAwesomeIcon size="2xl" icon={faBars} />
+        </a>
       </div>
     </nav>
   );
