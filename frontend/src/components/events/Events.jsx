340ce35f6dfd67b218e0ed346ddf2edbc6546610
Encoder012 <krishnaaggarwal012@gmail.com>
2024-02-01 13:57:13 +0530

    added events

--- frontend/src/components/events/Events.jsx
+++ frontend/src/components/events/Events.jsx
@@ -3,8 +3,27 @@ import "./Events.css"
 export default function Events() {
 
     return (
-        <div>
+        <div className="eventSection">
+            <h1>Events</h1>
+            <div className="eventInfo" >
+                <div className="eventLogo" >
+                    <img src="/events/event1Logo.png" alt=""></img>
+                </div>
+                <div className="eventName">
+                    <img src="/events/event1Name.png" alt=""></img>
 
+                </div>
+                <div className="eventImage">
+                    <img src="/events/event1Image.png" alt=""></img>
+                </div>
+            </div>
+            <div className="eventsTimeline" >
+                <div className="timelineInputs active"></div>
+                <div className="timelineInputs active"></div>
+                <div className="timelineInputs"></div>
+                <div className="timelineInputs"></div>
+                <div className="timelineInputs"></div>
+            </div>
         </div>
     );
 }
